1d1079b6de70b886d9a9472007b61c2efcf29e6f
System administrator <root@makondo.psy.jyu.fi>
2016-04-04 10:28:40 +0300

    Fix typo

--- configuration.nix
+++ configuration.nix
@@ -150,7 +150,7 @@
       # https://github.com/NixOS/nixpkgs/commit/5391882ebd781149e213e8817fba6ac3c503740c
       gpg-connect-agent /bye
       GPG_TTY=$(tty)
-      export GPT_TTY
+      export GPG_TTY
     '';
 
     windowManager.xmonad.enable = true;
