54f650a0cf997d2d1cc85ce2fa2be0afb76714a4
Asko Soukka <asko.soukka@iki.fi>
2019-09-13 09:07:27 +0300

    Update jupyter-env

--- overlays/custom/jupyter-env/default.nix
+++ overlays/custom/jupyter-env/default.nix
@@ -1,208 +1,52 @@
-{ pkgs, python36Packages}:
-
-with pkgs;
+{ pkgs ? import (builtins.fetchTarball {
+    # branches nixos-19.03
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/96151a48dd6662fb3f84bd16bbfe8a34f59d717a.tar.gz";
+    sha256 = "06cqc37yj23g3jbwvlf9704bl5dg8vrzqvs5y2q18ayg9sw61i6z";
+  }) {}
+}:
 
 let self = rec {
 
-  pythonPackages = python36Packages;
+  # python packages
 
-  # patches
+  pythonPackages = (import ./setup.nix {
+    inherit pkgs;
+    pythonPackages = pkgs.python3Packages;
+  }).pythonPackages;
 
-  robot_mode = ./robotframework.js;
+  # other packages
 
-  notebook = pythonPackages.notebook.overridePythonAttrs(old: { 
-    postInstall = ''
-      ls $out/${pythonPackages.python.sitePackages}/notebook/static/components/codemirror/mode/
-      mkdir $out/${pythonPackages.python.sitePackages}/notebook/static/components/codemirror/mode/robotframework
-      cp ${robot_mode} $out/${pythonPackages.python.sitePackages}/notebook/static/components/codemirror/mode/robotframework/robotframework.js
-    '';
-    doCheck = false;
-  });
-
-  # kernels
-
-  python36_with_packages = python36.buildEnv.override {
-    extraLibs = with python36Packages; [
-      iplantuml
-      ipykernel
-      ipywidgets
-      lti
-      magic
-      requests
-    ];
-  };
-
-  python36_kernel = stdenv.mkDerivation rec {
-    name = "python36";
-    buildInputs = [ python36_with_packages ];
-    json = builtins.toJSON {
-      argv = [ "${python36_with_packages}/bin/python3.6"
-               "-m" "ipykernel" "-f" "{connection_file}" ];
-      display_name = "Python 3.6";
-      language = "python";
-      env = { PYTHONPATH = ""; };
-    };
-    builder = builtins.toFile "builder.sh" ''
-      source $stdenv/setup
-      mkdir -p $out
-      cat > $out/kernel.json << EOF
-      $json
-      EOF
-    '';
-  };
-
-  lti = pythonPackages.buildPythonPackage rec {
-    pname = "lti";
-    version = "0.9.2";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "0k9d6ii7wf0iym0jspw0bpc4vvkk7xz7wibvmbzjjzn5rrnb3ak4";
-    };
-    propagatedBuildInputs = with pythonPackages; [
-      lxml
-      requests_oauthlib
-    ];
-    doCheck = false;
-  };
-
-  iplantuml = pythonPackages.buildPythonPackage rec {
-    pname = "IPlantUML";
-    version = "0.1.1";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "0p1w48iik5cl137b4xy3g5x52wl5b9fayxlr97bzd5v65dqxz1hg";
-    };
-    buildInputs = with pythonPackages; [ notebook ];
-    propagatedBuildInputs = [
-      plantweb
-      plantuml
-    ];
-    doCheck = false;
-  };
-
-  plantweb = pythonPackages.buildPythonPackage rec {
-    pname = "plantweb";
-    version = "1.1.0";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "1974022rpmwjg1amc7k8n3274bfj0vychd5c81r6q4mm02kysksz";
-    };
-    propagatedBuildInputs = with pythonPackages; [
-      docutils
-      requests
-    ];
-  };
-
-  robot_with_packages = buildEnv {
-    name = "robotkernel";
-    paths = [
-      (python36.buildEnv.override {
-        extraLibs = with python36Packages; [
-          ipykernel
-          ipywidgets
-          (import ./setup.nix {
-            inherit pkgs;
-            pythonPackages = python36Packages;
-          }).build
-        ];
-      })
-    ];
-  };
-
-  robot_kernel = stdenv.mkDerivation rec {
-    name = "robot";
-    buildInputs = [ robot_with_packages ];
-    json = builtins.toJSON {
-      argv = [ "${robot_with_packages}/bin/python3.6"
-               "-m" "robotkernel" "-f" "{connection_file}" ];
-      display_name = "Robot Framework";
-      language = "robotframework";
-      env = { PYTHONPATH = ""; };
-    };
-    builder = builtins.toFile "builder.sh" ''
-      source $stdenv/setup
-      mkdir -p $out
-      cat > $out/kernel.json << EOF
-      $json
-      EOF
-    '';
-  };
-
-  # extensions
-
-  rise = pythonPackages.buildPythonPackage rec {
-    pname = "rise";
-    version = "5.1.0";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "0b5rimnzd6zkgs7f286vr58a5rlzv275zd49xw48mn4dc06wfpz9";
-    };
-    buildInputs = [ notebook ];
-    postPatch = ''
-      sed -i "s|README.md'|README.md', encoding='utf-8'|" setup.py
-    '';
-  };
-
-  jupyter_nbextensions_configurator = pythonPackages.buildPythonPackage rec {
-    pname = "jupyter_nbextensions_configurator";
-    version = "0.3.0";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "11qq1di2gas8r302xpa0h2xndd5qgrz4a77myd2bd43c0grffa6b";
-    };
-    doCheck = false;
-    installFlags = [ "--no-dependencies" ];
-    propagatedBuildInputs = with pythonPackages; [ pyyaml ];
-  };
-
-  jupyter_contrib_nbextensions = pythonPackages.buildPythonPackage rec {
-    pname = "jupyter_contrib_nbextensions";
-    version = "0.3.3";
-    name = "${pname}-${version}";
-    src = pkgs.fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
-      sha256 = "0v730d5sqx6g106ii5r08mghbmbqi12pm6mpvjc0vsx703syd83f";
-    };
-    doCheck = false;
-    installFlags = [ "--no-dependencies" ];
-    propagatedBuildInputs = with pythonPackages; [ lxml ];
-  };
-
-  vim_binding = fetchFromGitHub {
+  vim_binding = pkgs.fetchFromGitHub {
     owner = "lambdalisue";
     repo = "jupyter-vim-binding";
     rev = "c9822c753b6acad8b1084086d218eb4ce69950e9";
     sha256 = "1951wnf0k91h07nfsz8rr0c9nw68dbyflkjvw5pbx9dmmzsa065j";
   };
 
-  # notebook
+  # jupyter
 
   jupyter = pythonPackages.jupyter.overridePythonAttrs (old: {
-    propagatedBuildInputs = old.propagatedBuildInputs ++ [
-      jupyter_contrib_nbextensions
-      jupyter_nbextensions_configurator
+    propagatedBuildInputs =
+    with pythonPackages; old.propagatedBuildInputs ++ [
+      graphviz
+      ipywidgets
+      jupyter-contrib-nbextensions
+      jupyter-nbextensions-configurator
+      jupyterlab
+      nbimporter
+      opencv3
       rise
+      tkinter
+      widgetsnbextension
     ];
-    postInstall = with pythonPackages; ''
-      mkdir -p $out/bin
-      ln -s ${jupyter_core}/bin/jupyter $out/bin
-      wrapProgram $out/bin/jupyter \
-        --prefix PYTHONPATH : "${notebook}/${python.sitePackages}:$PYTHONPATH" \
-        --prefix PATH : "${notebook}/bin:$PATH"
-    '';
   });
 
-  jupyter_nbconfig = stdenv.mkDerivation rec {
+  jupyter_nbconfig = pkgs.stdenv.mkDerivation rec {
     name = "jupyter";
     json = builtins.toJSON {
       load_extensions = {
+        "jupyter-js-widgets/extension" = true;
         "rise/main" = true;
-        "python-markdown/main" = true;
         "vim_binding/vim_binding" = true;
       };
       keys = {
@@ -215,7 +59,7 @@ let self = rec {
         };
       };
     };
-    builder = builtins.toFile "builder.sh" ''
+    builder = with pkgs; builtins.toFile "builder.sh" ''
       source $stdenv/setup
       mkdir -p $out
       cat > $out/notebook.json << EOF
@@ -224,36 +68,23 @@ let self = rec {
     '';
   };
 
-  jupyter_config_dir = stdenv.mkDerivation {
+  jupyter_config_dir = pkgs.stdenv.mkDerivation {
     name = "jupyter";
-    buildInputs = [
-      python36_kernel
-      robot_kernel
-      rise
-      vim_binding
-    ];
-    builder = writeText "builder.sh" ''
+    builder = with pythonPackages; with pkgs; writeText "builder.sh" ''
       source $stdenv/setup
-      mkdir -p $out/etc/jupyter/nbextensions
-      mkdir -p $out/etc/jupyter/kernels
       mkdir -p $out/etc/jupyter/migrated
-      ln -s ${python36_kernel} $out/etc/jupyter/kernels/${python36_kernel.name}
-      ln -s ${robot_kernel} $out/etc/jupyter/kernels/${robot_kernel.name}
-      ln -s ${jupyter_nbconfig} $out/etc/jupyter/nbconfig
-      ln -s ${jupyter_contrib_nbextensions}/${pythonPackages.python.sitePackages}/jupyter_contrib_nbextensions/nbextensions/* $out/etc/jupyter/nbextensions
-      ln -s ${rise}/${pythonPackages.python.sitePackages}/rise/static $out/etc/jupyter/nbextensions/rise
-      ln -s ${vim_binding} $out/etc/jupyter/nbextensions/vim_binding
-      cat > $out/etc/jupyter/jupyter_notebook_config.py << EOF
-      import os
-      import rise
-      c.KernelSpecManager.whitelist = {
-        '${python36_kernel.name}',
-        '${robot_kernel.name}'
-      }
-      c.NotebookApp.ip = os.environ.get('JUPYTER_NOTEBOOK_IP', 'localhost')
-      EOF
+      mkdir -p $out/share/jupyter/nbextensions
+      mkdir -p $out/share/jupyter/migrated
 
-      cat > $out/etc/jupyter/jupyter_nbconvert_config.py << EOF
+      ln -s ${jupyter_nbconfig} $out/share/jupyter/nbconfig
+      ln -s ${jupyter-contrib-nbextensions}/${pythonPackages.python.sitePackages}/jupyter-contrib-nbextensions/nbextensions/* $out/share/jupyter/nbextensions
+      ln -s ${rise}/${pythonPackages.python.sitePackages}/rise/static $out/share/jupyter/nbextensions/rise
+      ln -s ${vim_binding} $out/share/jupyter/nbextensions/vim_binding
+      ln -s ${widgetsnbextension}/share/jupyter/nbextensions/* $out/share/jupyter/nbextensions
+
+      echo "import rise" >> $out/share/jupyter/jupyter_notebook_config.py
+
+      cat > $out/share/jupyter/jupyter_nbconvert_config.py << EOF
       c = get_config()
       c.Exporter.preprocessors = ['jupyter_contrib_nbextensions.nbconvert_support.pre_pymarkdown.PyMarkdownPreprocessor']
       EOF
@@ -263,23 +94,21 @@ let self = rec {
 
 in with self;
 
-stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation rec {
   name = "jupyter";
-  env = buildEnv { name = name; paths = buildInputs; };
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
   builder = builtins.toFile "builder.sh" ''
     source $stdenv/setup; ln -s $env $out
   '';
   buildInputs = [
     jupyter
     jupyter_config_dir
-    geckodriver
-    chromedriver
-    plantweb
-    plantuml
-    pandoc
-    inkscape
-    (texlive.combine {
-      inherit (texlive)
+    pkgs.geckodriver
+    pkgs.chromedriver
+    pkgs.pandoc
+    pkgs.inkscape
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive)
       adjustbox
       collectbox
       enumitem
@@ -288,7 +117,7 @@ stdenv.mkDerivation rec {
       scheme-small
       ucs;
     })
-  ] ++ stdenv.lib.optionals stdenv.isLinux [ bash fontconfig tini ];
+  ];
   shellHook = ''
     mkdir -p $PWD/.jupyter
     export JUPYTER_CONFIG_DIR=${jupyter_config_dir}/etc/jupyter
--- overlays/custom/jupyter-env/setup.nix
+++ overlays/custom/jupyter-env/setup.nix
@@ -1,56 +1,32 @@
-{ pkgs ? import <nixpkgs> {}
-, pythonPackages ? pkgs.python3Packages
-, setup ? import (pkgs.fetchFromGitHub {
-    owner = "datakurre";
-    repo = "setup.nix";
-    rev = "3660563b961d2cfb81e197288ae9df2d0d9dc648";
-    sha256 = "173r6800bds7ignmcai1a5v264awl7y5smy0a3ccip6g4myb1759";
+{ pkgs ? import (builtins.fetchTarball {
+    # branches nixos-19.03
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/96151a48dd6662fb3f84bd16bbfe8a34f59d717a.tar.gz";
+    sha256 = "06cqc37yj23g3jbwvlf9704bl5dg8vrzqvs5y2q18ayg9sw61i6z";
+  }) {}
+, setup ? import (builtins.fetchTarball {
+    # tags v3.1.0
+    url = "https://github.com/nix-community/setup.nix/archive/129a384786f2d5985e1067e9b505f8cfc907e9fa.tar.gz";
+    sha256 = "1dp9bzivqaqqc2d9bnfy6jh37rfz6mvqaqbxy34l998y0khv5fpv";
   })
+, python ? "python3"
+, pythonPackages ? builtins.getAttr (python + "Packages") pkgs
+, requirements ? ./. + "/requirements-${python}.nix"
 }:
 
+let overrides = self: super: {
+  "pytest-mock" = super."pytest-mock".overridePythonAttrs(old: {
+    doCheck = false;
+  });
+  "json5" = super."json5".overridePythonAttrs(old: {
+    postPatch = "rm -r tests";
+  });
+  "RESTinstance" = super."RESTinstance".overridePythonAttrs(old: {
+    postInstall = "rm -f $out/bin/robot";
+  });
+}; in
+
 setup {
-  inherit pkgs pythonPackages;
-  overrides = self: super: {
-    "robotframework-SikuliLibrary" = pkgs.robotframework-sikulilibrary;
-    "robotframework-seleniumlibrary" = super."robotframework-seleniumlibrary".overridePythonAttrs(old: {
-      src = pkgs.fetchFromGitHub {
-        owner = "robotframework";
-        repo = "seleniumlibrary";
-        rev = "34cea295575c5f69e3325ec12b295a6167092acd";
-        sha256 = "07x6krwzvlba3087v38q53ww3spn1lmzva5kn9163l1m6kafpvii";
-      };
-    });
-    "backports.shutil-get-terminal-size" = pythonPackages.backports_shutil_get_terminal_size;
-    certifi = pythonPackages.certifi;
-    decorator = pythonPackages.decorator;
-    docutils = pythonPackages.docutils;
-    enum34 = pythonPackages.enum34;
-    idna = pythonPackages.idna;
-    ipykernel = pythonPackages.ipykernel;
-    ipython-genutils = pythonPackages.ipython_genutils;
-    ipython = pythonPackages.ipython;
-    jedi = pythonPackages.jedi;
-    jupyter-client = pythonPackages.jupyter_client;
-    jupyter-core = pythonPackages.jupyter_core;
-    jsonschema = pythonPackages.jsonschema;
-    parso = pythonPackages.parso;
-    pexpect = pythonPackages.pexpect;
-    pillow = pythonPackages.pillow;
-    prompt-toolkit = pythonPackages.prompt_toolkit;
-    ptyprocess = pythonPackages.ptyprocess;
-    pygments = pythonPackages.pygments;
-    py = pythonPackages.py;
-    python-dateutil = pythonPackages.dateutil;
-    pyzmq = pythonPackages.pyzmq;
-    requests = pythonPackages.requests;
-    singledispatch = pythonPackages.singledispatch;
-    six = pythonPackages.six;
-    tornado = pythonPackages.tornado;
-    traitlets = pythonPackages.traitlets;
-    urllib3 = pythonPackages.urllib3;
-    wcwidth = pythonPackages.wcwidth;
-    chardet = pythonPackages.chardet;
-  };
+  inherit pkgs pythonPackages overrides;
   src = ./.;
-  propagatedBuildInputs = with pkgs; [ geckodriver pythonPackages.opencv3 ];
+  requirements = requirements;
 }
