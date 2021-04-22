85ec9cd4e1b8689f101b7d072bc1809ca5a5adaa
Asko Soukka <asko.soukka@iki.fi>
2021-04-22 13:33:49 +0300

    Package popular plugins with camunda-modeler

--- overlays/custom/camunda-modeler/default.nix
+++ overlays/custom/camunda-modeler/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, libXScrnSaver, makeWrapper, fetchurl, wrapGAppsHook, glib, gtk3, unzip, atomEnv, libuuid, at-spi2-atk, at-spi2-core, nodePackages, autoPatchelfHook, gcc-unwrapped, libdrm, mesa, libxkbcommon }:
+{ stdenv, libXScrnSaver, makeWrapper, fetchurl, wrapGAppsHook, glib, gtk3, unzip, atomEnv, libuuid, at-spi2-atk, at-spi2-core, nodePackages, autoPatchelfHook, gcc-unwrapped, libdrm, mesa, libxkbcommon, adoptopenjdk-jre-hotspot-bin-11 }:
 
 let
 
@@ -6,6 +6,31 @@ let
   electron = mkElectron "7.3.3" {
     x86_64-linux = "a947228a859149bec5bd937f9f3c03eb0aa4d78cfe4dfa9aead60d3646a357f9";
     x86_64-darwin = "e081436abef52212065f560ea6add1c0cd13d287a1b3cc76b28d2762f7651a4e";
+  };
+
+  camunda-modeler-plugins = fetchurl {
+    url = "https://github.com/camunda/camunda-modeler-plugins/archive/3659fb8dac82098fd8de98d50d03f0d33eea7556.tar.gz";
+    sha256 = "0l64a7zq7z1vx2bjbrff9xn1g1c798zxf41pjnjggjwm55kfqvh0";
+  };
+
+  camunda-modeler-linter-plugin = fetchurl {
+    url = "https://github.com/camunda/camunda-modeler-linter-plugin/archive/c506ee6b250871c9b7785e9a1ff9fa9fe4334c85.tar.gz";
+    sha256 = "080x1yjic2lhs5mqsbw3n2vxsgyadjgrflqjx20grq1vv3vjf5s1";
+  };
+
+  camunda-modeler-robot-plugin = fetchurl { 
+    url = "https://github.com/datakurre/camunda-modeler-robot-plugin/archive/d9cf521699cfac82ad892c8c9951705438cb0fb5.tar.gz";
+    sha256 = "0gin2icbr74a6ppcs7yj0dxl6na8m98b5whvgxglvm8r0qwzfbgj";
+  };
+
+  bpmn-js-token-simulation-plugin = fetchurl { 
+    url = "https://github.com/bpmn-io/bpmn-js-token-simulation-plugin/archive/b856b708362a2022180316a8b4c84ae7122bcf63.tar.gz";
+    sha256 = "0b005201c5ec346f542c6f556c2d7579a021707fd7f0701f3f7819b58a8aeb7b";
+  };
+
+  dmn-testing-plugin = fetchurl { 
+    url = "https://github.com/bpmn-io/dmn-testing-plugin/archive/ca25586d607e3bd04357ee1546424f859591e0b8.tar.gz";
+    sha256 = "4a48bfeeac46baaa5731ae2472c564d6c154bf5ca7716b059d0ce1b47ecaa26d";
   };
 
 in
@@ -24,15 +49,27 @@ stdenv.mkDerivation rec {
     mkdir build
     cd build
     asar extract ../resources/app.asar .
+    substituteInPlace ./lib/index.js \
+      --replace "let resourcesPaths = [" \
+                "let resourcesPaths = [\"$out/var/lib/camunda/resources\","
     find . -name "grpc_node.node"
     if [ -f node_modules/grpc/src/node/extension_binary/electron-v7.3-linux-x64-glibc/grpc_node.node ]; then
       autoPatchelf node_modules/grpc/src/node/extension_binary/electron-v7.3-linux-x64-glibc/grpc_node.node;
     fi
     asar pack . ../app.asar
     cd ..
-    mkdir -p $out/var/lib/camunda $out/bin
+    mkdir -p $out/var/lib/camunda/resources/plugins $out/bin
     cp app.asar $out/var/lib/camunda
+    cd $out/var/lib/camunda/resources/plugins
+    tar xzvf ${dmn-testing-plugin}
+    tar xzvf ${camunda-modeler-robot-plugin}
+    tar xzvf ${bpmn-js-token-simulation-plugin}
+    tar xzvf ${camunda-modeler-linter-plugin}
+    tar xzvf ${camunda-modeler-plugins}
+    mv camunda-modeler-plugins*/camunda-transaction-boundaries-plugin .
+    rm -r camunda-modeler-plugins*
     makeWrapper ${electron}/bin/electron $out/bin/camunda-modeler \
-      --add-flags "$out/var/lib/camunda/app.asar"
+      --add-flags "$out/var/lib/camunda/app.asar" \
+      --prefix PATH : "${adoptopenjdk-jre-hotspot-bin-11}/bin"
   '';
 }
