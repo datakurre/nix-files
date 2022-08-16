c73b4ffc13c105911c9f09a06f157f02ec95a9a3
Asko Soukka <asko.soukka@iki.fi>
2022-08-16 09:13:29 +0300

    camunda-modeler: 5.2.0

--- overlays/custom/pkgs/camunda-modeler/default.nix
+++ overlays/custom/pkgs/camunda-modeler/default.nix
@@ -25,8 +25,8 @@ let
   };
 
   camunda-modeler-robot-plugin = fetchurl {
-    url = "https://github.com/datakurre/camunda-modeler-robot-plugin/archive/d9cf521699cfac82ad892c8c9951705438cb0fb5.tar.gz";
-    sha256 = "0gin2icbr74a6ppcs7yj0dxl6na8m98b5whvgxglvm8r0qwzfbgj";
+    url = "https://github.com/datakurre/camunda-modeler-robot-plugin/archive/287307f1c4951e937281a0f00d5857e01257b7c0.tar.gz";
+    sha256 = "sha256-Msa1ZD6PPvKZQh3A/3m5kczCAGmmSCAV+r6NdgmGKak=";
   };
 
   camunda-modeler-tooltip-plugin = fetchurl {
@@ -51,10 +51,10 @@ let
 
   asar = stdenv.mkDerivation rec {
     name = "camunda-modeler-${version}-asar";
-    version = "5.0.0";
+    version = "5.2.0";
     src = fetchurl {
       url = "https://github.com/camunda/camunda-modeler/releases/download/v${version}/camunda-modeler-${version}-linux-x64.tar.gz";
-      sha256 = "11gkcb209dgnmavn113b91dywh4b1n4i0a7xnhkc4angn8y22amp";
+      sha256 = "sha256-ycvs4bf7yqYMW8Xjb/kEHiJv6vuV2jQRxHPNzZoEwjM=";
     };
     nativeBuildInputs = [ nodePackages.asar autoPatchelfHook gcc-unwrapped ];
     installPhase = ''
@@ -66,7 +66,7 @@ in
 
 stdenv.mkDerivation rec {
   name = "camunda-modeler-${version}";
-  version = "5.0.0";
+  version = "5.2.0";
   src = asar;
   unpackPhase = "";
   nativeBuildInputs = [ electron makeWrapper nodePackages.asar autoPatchelfHook gcc-unwrapped ];
