e2ea88c45755c2898ae25f0b56d081e2e63be810
Asko Soukka <asko.soukka@iki.fi>
2025-03-10 07:51:23 +0200

    Add gitchlog-helper

--- modules/programs-git.nix
+++ modules/programs-git.nix
@@ -25,7 +25,38 @@
     };
     home = {
       file.".gitconfig".source = ./programs-git-gitconfig;
-      packages = [ pkgs.git ];
+      packages = [
+        pkgs.git
+        (pkgs.stdenv.mkDerivation {
+          name = "gitlog";
+          builder = builtins.toFile "builder.sh" ''
+            source $stdenv/setup
+            mkdir -p $out/bin
+            cat > $out/bin/gitlog << EOF
+#!/usr/bin/env bash
+git log \
+--pretty=format:"- %s%n  [%an]" "\`git describe --tags|grep -o '^[^-]*'\`"..HEAD
+EOF
+            chmod u+x $out/bin/gitlog
+          '';
+        })
+        (pkgs.stdenv.mkDerivation {
+          name = "gitclog";
+          builder = builtins.toFile "builder.sh" ''
+            source $stdenv/setup
+            mkdir -p $out/bin
+            cat > $out/bin/gitclog << EOF
+#!/usr/bin/env bash
+head -n 6 \$1 \
+>> \$1.new && gitlog \
+>> \$1.new && tail -n +8 \$1 \
+>> \$1.new && mv \$1.new \$1
+EOF
+            chmod u+x $out/bin/gitclog
+          '';
+        })
+
+      ];
     };
   };
 }
